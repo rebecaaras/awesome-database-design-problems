275217fdc702d45fd754c09725748920f974f95e
rebecaaras <rebecaaraujo2013@gmail.com>
2026-02-26 21:46:00 -0300

    fix: remove GENERATED AS IDENTITY from author_id  in book_author table so the correct ids are added in each row, add seed data to book_author

--- problems/library-system/schema/03_book_author.sql
+++ problems/library-system/schema/03_book_author.sql
@@ -1,5 +1,5 @@
 CREATE TABLE public.book_author (
-    author_id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY,
+    author_id INTEGER NOT NULL,
     book_id INTEGER NOT NULL,
     CONSTRAINT book_author_pkey PRIMARY KEY (author_id, book_id),
     CONSTRAINT fk_author_id FOREIGN KEY (author_id) REFERENCES public.author (author_id) ON DELETE CASCADE,
